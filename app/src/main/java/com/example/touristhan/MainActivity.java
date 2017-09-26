e5f500e48a61cef540e749654a04cc96ec48cd77
Aditya Kaushik <adityakaushik471@gmail.com>
2017-09-26 16:32:25 +0530

    First Commit

---
+++ app/src/main/java/com/example/touristhan/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.touristhan;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)             Toolbar         toolbar;
+    @BindView(R.id.navigation_view)     NavigationView  nvView;
+    @BindView(R.id.drawer_layout)       DrawerLayout    mDraweLayout;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+       //  Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mActionBarDrawerToggle.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        mActionBarDrawerToggle = setupDrawerToggle();
+        setupNavDrawerContent(nvView);
+
+    }
+
+    /**
+     * For setting up the navigation panel of drawar layout.
+     * @param navigationView The view passed
+     */
+    private void setupNavDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Helper method for selecting the navigation item.
+     * @param item The item clicked on
+     */
+    private void selectDrawerItem(MenuItem item){
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mActionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    /**
+     * Helper method for setting up the heamburgur icon, This is done by tieing out the hamburger icon
+     * and toolbar together.
+     *
+     * @return instance of class, that deals with hamburger icon, click...
+     */
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDraweLayout, toolbar, R.string.nav_bar_open, R.string.nav_bar_close);
+    }
+}
